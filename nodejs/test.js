8853d63f84edfcff24d46993031d15ad6c32a75d
SU <rumex13@naver.com>
2017-04-30 23:16:23 +0900

    노드 js

---
+++ nodejs/test.js
@@ -0,0 +1,7 @@
+var winston = require('winston'); // 로그 처리 모듈
+var winstonDaily = require('winston-daily-rotate-file'); // 로그 일별 처리 모듈
+var moment = require('moment'); // 시간 처리 모듈
+
+function timeStampFormat() {
+    return moment().format('YYYY-MM-DD HH:mm:ss.SSS ZZ');
+};
