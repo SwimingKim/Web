9fcb95941ff9d90798bacce2d2c7c4d965ccf61a
SU <rumex13@naver.com>
2017-06-29 15:58:20 +0900

    채팅 프로젝트

---
+++ nodejs/ChatExample/config/config.js
@@ -0,0 +1,29 @@
+
+/*
+ * 설정
+ */
+
+module.exports = {
+	server_port: 3000,
+	db_url: 'mongodb://localhost:27017/local',
+	db_schemas: [
+	    {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'}
+	],
+	route_info: [
+	],
+    facebook: {		// passport facebook
+		clientID: '1911500929135515',
+		clientSecret: '23444c5da369e48300899c937df75125',
+		callbackURL: 'http://localhost:3000/auth/facebook/callback'
+	},
+	twitter: {		// passport twitter
+		clientID: 'id',
+		clientSecret: 'secret',
+		callbackURL: '/auth/twitter/callback'
+	},
+	google: {		// passport google
+		clientID: 'id',
+		clientSecret: 'secret',
+		callbackURL: '/auth/google/callback'
+	}
+}
