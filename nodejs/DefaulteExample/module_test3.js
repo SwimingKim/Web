0995ee25ff88bba62fbe54af8133cb93772bac67
SU <rumex13@naver.com>
2017-10-02 15:57:24 +0900

    Delete node

--- nodejs/DefaulteExample/module_test3.js
+++
@@ -1,8 +0,0 @@
-// require() 메소드는 객체를 반환함
-var user = require('./user3');
-
-function showUser() {
-    return user.getUser().name + ', ' + user.group.name;
-}
-
-console.log('사용자 정보 : %s', showUser());
--- nodejs/DefaulteExample/module_test4.js
+++
@@ -1,8 +0,0 @@
-// require() 메소드는 함수를 반환함
-var user = require('./user4');
-
-function showUser() {
-    return user().name + ', ' + 'No Group';
-}
-
-console.log('사용자 정보 : %s', showUser());
